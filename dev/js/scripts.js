e3d11371b99337a25f731529b2ac684117406004
Julia Kimball <juliakimball6@gmail.com>
2021-11-22 02:21:06 -0500

    new new
    new

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,41 @@
-// import { gsap } from "gsap";
+
+import { gsap } from "gsap";
+
+import { GSDevTools } from "gsap/GSDevTools";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { CustomEase } from "gsap/CustomEase";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+
+
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, CustomEase, MorphSVGPlugin);
+
+MorphSVGPlugin.convertToPath();
+
+
+const mainTL = gsap.timeline();
+
+function Frame_78(){
+const tl = gsap.timeline({repeat: 0});
+
+tl.to("#Vector1", {duration: .35, morphSVG:"#Vector2"});
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+mainTL
+.add(Frame_78())
+
+
+
+GSDevTools.create();
